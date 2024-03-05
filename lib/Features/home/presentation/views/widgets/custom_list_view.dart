b431be9a7bd92a9172817b2da8ed7172fbc6612f
alaakorda <alaazakaria436@gmail.com>
2024-03-05 22:08:24 +0200

    add list view

---
+++ lib/Features/home/presentation/views/widgets/custom_list_view.dart
@@ -0,0 +1,22 @@
+import 'package:bookly/Features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class CustomListView extends StatelessWidget {
+  const CustomListView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+       height: MediaQuery.of(context).size.width * 0.5,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal:8.0),
+            child: CustomListViewItem(),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:bookly/Features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:bookly/Features/splash/presentation/view_model/views/widgets/custom_list_view_item.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/custom_list_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -9,7 +9,7 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
-        CustomListViewItem(),
+         CustomListView(),
       ],
     );
   }
