6ff6369c39a6c711fcf176ee80fa3eb8ed15ef0e
alaakorda <alaazakaria436@gmail.com>
2024-03-09 02:15:52 +0200

    update text style

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -20,7 +20,7 @@ class HomeViewBody extends StatelessWidget {
           ),
           Text(
             'Best Seller',
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
           ),
           BestSellerListViewItem(),
         ],
