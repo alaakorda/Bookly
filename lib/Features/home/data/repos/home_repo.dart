29fc493178417e86c3a1cca2ea23231cd55623c7
alaakorda <alaazakaria436@gmail.com>
2024-03-18 03:05:32 +0200

    create failures file

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,5 +1,8 @@
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
 abstract class HomeRepo{
-  fetchBestSellerBooks();
-  fetchFeatureBooks();
+  Future<Either<Failures,List<BookModel>>>fetchBestSellerBooks();
+  Future<Either<Failures,List<BookModel>>>fetchFeatureBooks();
 
 }
