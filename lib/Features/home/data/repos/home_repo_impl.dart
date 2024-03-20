2a4214a91d4d049e0707eef21e8b228d22d8818a
alaakorda <alaazakaria436@gmail.com>
2024-03-20 08:51:45 +0200

    create failure file

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly/Features/home/data/repos/home_repo.dart';
 import 'package:bookly/core/errors/failures.dart';
 import 'package:bookly/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -20,12 +21,29 @@ class HomeRepoImpl implements HomeRepo {
       }
       return right(books);
     } catch (e) {
-      return left(ServerFailure());
+      if(e is DioError){
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
     }
   }
 
   @override
-  Future<Either<Failures, List<BookModel>>> fetchFeatureBooks() {
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fetchFeatureBooks() async{
+      try {
+      var data = await apiService.get(
+          endpoint:
+              'volumes?Filtering=free-ebooks&q=subject:Programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if(e is DioError){
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,2 +1,39 @@
-abstract class Failures{}
-class ServerFailure extends Failures{}
+import 'package:dio/dio.dart';
+
+abstract class Failures {}
+
+class ServerFailure extends Failures {
+  final String errorMassage;
+
+  ServerFailure(this.errorMassage);
+  factory ServerFailure.fromDioException(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection Timeout');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send Timeout');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive Timeout');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad Certificate');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromReponse(
+            dioError.response!.statusCode!, dioError.response!.data);
+      case DioExceptionType.cancel:
+      return ServerFailure('Request Cancelled');
+      
+      case DioExceptionType.connectionError:
+        return 
+      case DioExceptionType.unknown:
+        // TODO: Handle this case.
+    }
+  }
+  factory ServerFailure.fromReponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404)
+      return ServerFailure('Not Found');
+    else if (statusCode == 500) return ServerFailure('Internal Server Error');
+    return ServerFailure('Oops! Something went wrong!');
+  }
+}
